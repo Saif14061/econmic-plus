9e825a7a0c95346041d986348d0aadc242247871
Saif <saif.abbas0192@gmail.com>
2026-03-12 22:46:40 +0000

    Adding tables from SQL

--- processor/processor.py
+++ processor/processor.py
@@ -2,3 +2,36 @@ from kafka import KafkaConsumer
 import json
 from textblob import TextBlob
 import psycopg2
+
+consumer = KafkaConsumer("news","stocks",bootstrap_servers='localhost:9092')
+
+conn = psycopg2.connect(host="localhost",port="5432",dbname="economicpulse",user="saif",password="password123")
+cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS News (
+        
+        ID           SERIAL PRIMARY KEY,
+        HEADLINE     TEXT,
+        SOURCE       TEXT,
+        SENTIMENT_SCORE FLOAT,
+        TIMESTAMP    TIMESTAMP DEFAULT NOW(),
+        
+               
+        
+               
+    )
+""")
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Stocks (
+        
+        ID         SERIAL PRIMARY KEY,
+        TICKER     TEXT,
+        PRICE      FLOAT,
+        TIMESTAMP  TIMESTAMP DEFAULT NOW(),
+        
+               
+        
+               
+    )
+""")
